e3b16f370014596bd60e5f1cf1103dcea4a22051
GarciaY <201911021@gordoncollege.edu.ph>
2023-03-04 22:49:46 +0800

    modal-update

---
+++ js/residence-modal.js
@@ -0,0 +1,19 @@
+let btnModal = document.querySelector(".btnModal");
+let box = document.querySelector(".box");
+let body = document.querySelector("body");
+let mainTop = document.querySelector(".main-top");
+let main = document.querySelector(".main");
+let attendance = document.querySelector(".attendance");
+let close = document.querySelector(".close");
+
+btnModal.addEventListener("click", ()=>{
+    
+    box.style.display="block";
+    
+});
+
+close.addEventListener("click", ()=>{
+    
+    box.style.display="none";
+  
+});
